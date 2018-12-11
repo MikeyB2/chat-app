9a2c8c46991b9e431a32260332ba273c538cca2d
Mike Bryant <mike.bryant.jr@icloud.com>
2018-12-11 14:57:02 -0700

    css

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -4,7 +4,6 @@ class SendMessageForm extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>Send Message Form Component</h1>
 				<form className="send-message-form">
 					<input
 						placeholder="SendMessageForm"
